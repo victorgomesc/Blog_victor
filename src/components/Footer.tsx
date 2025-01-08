927f05e4615ff198ca37d1fda028228af540f728
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-08 20:40:24 -0300

    refact: footer componente

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,53 +1,73 @@
 import { Github, Instagram, Linkedin, Mail, Phone } from "lucide-react";
 import Link from "next/link";
 
-
 export default function Footer() {
-    return(
-        <footer className=" h-56 mt-36 w-full bg-gray-800 object-bottom">
-          <div className="flex flex-row">
-             <div className="flex ml-20 w-1/5 flex-col mt-3 justify-center">
-               <Link href="https://www.instagram.com/victor_gomesc/">
-                <p className="text-white mt-2 flex gap-2"><Instagram size={20} />: Victor_gomesc</p>
-               </Link>
-                <p className="text-white mt-2 flex gap-2"><Mail size={20} />: victorgomesdacosta18@gmail.com</p>
-                <p className="text-white mt-2 flex gap-2"><Phone size={20} />: +55(88)981437585</p>
-               <Link href="https://www.linkedin.com/in/victor-gomes-da-costa-123a90241">
-                <p className="text-white mt-2 flex gap-2"><Linkedin size={20} />: Linkedin</p>
-               </Link>
-               <Link href="https://github.com/victorgomesc">
-                <p className="text-white mt-2 flex gap-2"><Github size={20} />: GitHub</p>
-               </Link>
-             </div>
-             <div className="flex ml-20 w-1/5 flex-col mt-3 justify-center">
-                <p className="text-white mt-2 font-bold flex gap-2">Parcerias</p>
-                <Link href="https://www.quixada.ufc.br/">
-                  <p className="text-white mt-2 flex gap-2">Universidade federal do Ceara</p>
-                </Link>
+  const contactInfo = [
+    { href: "https://www.instagram.com/victor_gomesc/", icon: <Instagram size={20} />, text: "Victor_gomesc" },
+    { icon: <Mail size={20} />, text: "victorgomesdacosta18@gmail.com" },
+    { icon: <Phone size={20} />, text: "+55(88)981437585" },
+    { href: "https://www.linkedin.com/in/victor-gomes-da-costa-123a90241", icon: <Linkedin size={20} />, text: "Linkedin" },
+    { href: "https://github.com/victorgomesc", icon: <Github size={20} />, text: "GitHub" },
+  ];
 
-                <Link href="#">
-                  <p className="text-white mt-2 flex gap-2">Sistem Coop</p>
-                </Link>
+  const partnerships = [
+    { href: "https://www.quixada.ufc.br/", text: "Universidade Federal do Ceará" },
+    { href: "#", text: "Sistem Coop" },
+    { href: "https://www.ec.quixada.ufc.br/", text: "Engenharia de Computação" },
+    { href: "#", text: "Coop Tech" },
+  ];
 
-                <Link href="https://www.ec.quixada.ufc.br/">
-                  <p className="text-white mt-2 flex gap-2">Engenharia de computação</p>
-                </Link>
+  const footerDetails = [
+    { text: "09/05/2024" },
+    { text: "Versão 0.0.1" },
+    { text: "Autor: Victor Gomes" },
+    { text: "VGC Tec" },
+  ];
 
-                <Link href="#">
-                  <p className="text-white mt-2 flex gap-2">Coop Tech</p>
-                </Link>
-             </div>
-             <div className="flex ml-20 w-1/5 flex-col mt-3 justify-center">
-                <p className="text-white mt-2 flex gap-2">09/05/2024</p>
-                <p className="text-white mt-2 flex gap-2">Versão 0.0.1</p>
-                <p className="text-white mt-2 flex gap-2">Autor: Victor Gomes</p>
-                <p className="text-white mt-2 flex gap-2">VGC Tec</p>
-             </div>
-             <div className="flex ml-20 w-1/5 flex-col mt-3 justify-center">
-               <p className="text-lg text-white italic">Primeiro, lembre de olhar para as estrelas e não para seus pés.</p>
-               <p className="text-lg text-white mt-2">Stephen Hawking</p>
-             </div>
-          </div>
-        </footer>
-    )
+  const quote = {
+    text: "Primeiro, lembre de olhar para as estrelas e não para seus pés.",
+    author: "Stephen Hawking",
+  };
+
+  return (
+    <footer className="h-56 mt-36 w-full bg-gray-800">
+      <div className="flex flex-row">
+        {/* Contact Info */}
+        <div className="flex ml-20 w-1/5 flex-col mt-3 justify-center">
+          {contactInfo.map((item, index) => (
+            <Link key={index} href={item.href || "#"} aria-label={item.text}>
+              <p className="text-white mt-2 flex gap-2">
+                {item.icon}: {item.text}
+              </p>
+            </Link>
+          ))}
+        </div>
+
+        {/* Partnerships */}
+        <div className="flex ml-20 w-1/5 flex-col mt-3 justify-center">
+          <p className="text-white mt-2 font-bold">Parcerias</p>
+          {partnerships.map((item, index) => (
+            <Link key={index} href={item.href} aria-label={item.text}>
+              <p className="text-white mt-2">{item.text}</p>
+            </Link>
+          ))}
+        </div>
+
+        {/* Footer Details */}
+        <div className="flex ml-20 w-1/5 flex-col mt-3 justify-center">
+          {footerDetails.map((detail, index) => (
+            <p key={index} className="text-white mt-2">
+              {detail.text}
+            </p>
+          ))}
+        </div>
+
+        {/* Quote */}
+        <div className="flex ml-20 w-1/5 flex-col mt-3 justify-center">
+          <p className="text-lg text-white italic">{quote.text}</p>
+          <p className="text-lg text-white mt-2">{quote.author}</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
